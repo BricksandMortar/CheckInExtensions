8be2629afa80f18bcb3737c01a996b469bc4a033
Arran France <arranf@gmail.com>
2016-07-06 00:37:29 +0100

    Fix ignored elements

---
+++ script/ciinstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+cd ..
+git clone --depth=3 --branch=gh-pages https://github.com/BricksandMortar/plugin-doc-template.git
+rm -rf plugin-doc-template/.git
+cp -r -n -p plugin-doc-template/* $TRAVIS_BUILD_DIR/
+cd $TRAVIS_BUILD_DIR
+#gem install jekyll html-proofer github-pages wdm
+bundle install
+sed -i -e 's/$VERSION_NUMBER/'$TRAVIS_BUILD_NUMBER'/g' ./_layouts/*.html
+sed -i -e  's/'#baseurl'/baseurl/' ./_config.yml
