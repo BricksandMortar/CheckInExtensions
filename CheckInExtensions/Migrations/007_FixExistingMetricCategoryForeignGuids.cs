874e4283f86a162b23a51f50a271a283d9c309ee
Arran France <arranf@gmail.com>
2016-10-28 19:58:07 +0100

    Fix issue with duplicate metrics and categories

---
+++ CheckInExtensions/Migrations/007_FixExistingMetricCategoryForeignGuids.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock.Plugin;
+
+namespace com.bricksandmortarstudio.checkinextensions.Migrations
+{
+    [MigrationNumber( 7, "1.4.0" )]
+    class FixExistingMetricCategoryForeignGuids : Migration
+    {
+        public override void Up()
+        {
+            Sql( @"
+            DECLARE @metricCategoryEntityId int;
+            SET @metricCategoryEntityId = (SELECT TOP 1 [Id] 
+						            FROM [EntityType]
+						            WHERE [EntityType].[Name] = 'Rock.Model.MetricCategory');
+
+            UPDATE [Category]
+            SET [Category].[ForeignGuid] = (SELECT TOP 1 [Guid]
+
+            FROM (SELECT *
+		            FROM [GroupType]
+		            WHERE [Id] = (SELECT TOP 1 [GroupTypeId]
+						            FROM [Group]
+						            WHERE [Guid] = (SELECT TOP 1 [ForeignGuid]
+										            FROM [Metric]
+										            WHERE [Id] IN (SELECT [MetricId]
+														            FROM [MetricCategory]
+														            WHERE [CategoryId] = [Category].[Id]
+														            )
+										            )
+						            )
+		            ) AS f)
+            WHERE [EntityTypeId] = @metricCategoryEntityId
+            " );
+        }
+
+        public override void Down()
+        {
+           
+        }
+    }
+}
---
+++ CheckInExtensions/Migrations/008_RemoveDuplicateMetricsRecursively.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock.Plugin;
+
+namespace com.bricksandmortarstudio.checkinextensions.Migrations
+{
+    [MigrationNumber( 8, "1.4.0" )]
+    class RemoveDuplicateMetricsRecursively : Migration
+    {
+        public override void Up()
+        {
+            Sql( @"
+            DECLARE @metricCategoryEntityId int;
+SET @metricCategoryEntityId = (SELECT TOP 1 [Id] 
+						FROM [EntityType]
+						WHERE [EntityType].[Name] = 'Rock.Model.MetricCategory');
+
+
+WITH q AS
+        (
+        SELECT  id
+        FROM    [Category]
+        WHERE [Category].Id NOT IN (SELECT [MetricCategory].CategoryId 
+						            FROM [MetricCategory] 
+						            WHERE (
+							            SELECT Count(*) 
+							            FROM [MetricValue] 
+							            WHERE [MetricValue].MetricId = [MetricCategory].[MetricId]) > 0)
+            AND [Category].EntityTypeId = @metricCategoryEntityId AND Category.[Guid] != 'A0832CDB-CB65-4DFD-9D1B-4F702D8BB64F'
+
+        UNION ALL
+        SELECT  c.id
+        FROM    q
+        JOIN    [Category] as c
+        ON      c.ParentCategoryId = q.id
+        )
+DELETE
+FROM    [Category]
+WHERE   EXISTS
+        (
+        SELECT  id
+        INTERSECT
+        SELECT  id
+        FROM    q
+        )
+
+		            DELETE
+            FROM [Metric]
+            WHERE ((SELECT COUNT(*) FROM [MetricValue] WHERE [MetricValue].MetricId = [Metric].Id) = 0 AND [Metric].[Description] LIKE '%Headcount%')
+
+            " );
+        }
+
+        public override void Down()
+        {
+           
+        }
+    }
+}
