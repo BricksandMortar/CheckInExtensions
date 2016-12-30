07a95023f03a5d40d6ba1eef12f6a69fec26dd14
Arran France <arranf@gmail.com>
2016-12-30 18:40:26 +0000

    Add helper to get check in templates

---
+++ CheckInExtensions/Utils/CheckInGroupsHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+
+namespace com.bricksandmortarstudio.checkinextensions.Utils
+{
+    public static class CheckInGroupsHelper
+    {
+        public static IEnumerable<GroupType> GetCheckInTemplatesGroupTypes(RockContext rockContext = null)
+        {
+            if ( rockContext == null )
+            {
+                rockContext = new RockContext();
+            }
+            var groupTypeService = new GroupTypeService(rockContext);
+            int checkInTemplateId = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.GROUPTYPE_PURPOSE_CHECKIN_TEMPLATE ).Id;
+            return groupTypeService.Queryable().AsNoTracking().Where( g => g.GroupTypePurposeValueId == checkInTemplateId );
+        }
+    }
+}
