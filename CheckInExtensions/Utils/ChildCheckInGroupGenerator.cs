0d9bdb32fafd46fc5e7ca54b5c155daf78528c49
Arran France <arranf@gmail.com>
2017-09-26 12:00:02 +0100

    Ensure groups nested further than two levels display

--- CheckInExtensions/Utils/ChildCheckInGroupGenerator.cs
+++ CheckInExtensions/Utils/ChildCheckInGroupGenerator.cs
@@ -9,27 +9,27 @@ namespace com.bricksandmortarstudio.checkinextensions.Utils
     {
         /// Given a list of groupTypes this classes returns their child groups that are related to check-in via recursively expanding the tree avoiding loops
 
-        private int _checkInTemplateId;
+        private int _checkInFilterId;
         private readonly List<int> _seenGroupTypeIds = new List<int>();
         private readonly List<Group> _groups = new List<Group>();
 
         public List<Group> Get(IEnumerable<GroupType> groupTypes)
         {
-            _checkInTemplateId = DefinedValueCache.Read(Rock.SystemGuid.DefinedValue.GROUPTYPE_PURPOSE_CHECKIN_TEMPLATE).Id;
+            _checkInFilterId = DefinedValueCache.Read(Rock.SystemGuid.DefinedValue.GROUPTYPE_PURPOSE_CHECKIN_FILTER).Id;
             GetValidGroups(groupTypes);
             return _groups;
         }
 
         public List<Group> Get( GroupType groupType )
         {
-            _checkInTemplateId = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.GROUPTYPE_PURPOSE_CHECKIN_TEMPLATE ).Id;
+            _checkInFilterId = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.GROUPTYPE_PURPOSE_CHECKIN_TEMPLATE ).Id;
             GetValidGroup( groupType );
             return _groups;
         }
 
         public List<Group> Get( IEnumerable<int> groupTypeIds )
         {
-            _checkInTemplateId = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.GROUPTYPE_PURPOSE_CHECKIN_TEMPLATE ).Id;
+            _checkInFilterId = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.GROUPTYPE_PURPOSE_CHECKIN_TEMPLATE ).Id;
             var groupTypeService = new GroupTypeService( new Rock.Data.RockContext() );
             var groupTypes = groupTypeService.GetByIds( groupTypeIds.ToList() );
             GetValidGroups( groupTypes );
@@ -57,7 +57,7 @@ namespace com.bricksandmortarstudio.checkinextensions.Utils
             {
                 GetValidGroups(
                     groupType.ChildGroupTypes.Where(
-                        g => ( g.GroupTypePurposeValueId == _checkInTemplateId || g.GroupTypePurposeValueId == null ) && !_seenGroupTypeIds.Contains( g.Id ) )
+                        g => ( g.GroupTypePurposeValueId != _checkInFilterId || g.GroupTypePurposeValueId == null ) && !_seenGroupTypeIds.Contains( g.Id ) )
                         .ToList() );
             }
         }
